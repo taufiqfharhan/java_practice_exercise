2ac97a8bd2f6c0e7185701dc821c9cd0767727d9
Taufiq Fharhan <m.taufiq.fharhan@gmail.com>
2025-03-29 05:45:35 +0800

    First java program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        //This is may first
+
+        System.out.println("I like spaghetti!");
+        System.out.println("It's really awesome!");
+        System.out.println("Buy me spaghetti!");
+        
+    }
+}
