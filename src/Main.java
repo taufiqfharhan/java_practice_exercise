e1e23a11e6b03496c4184dd28a6b302a0c1bc664
Taufiq Fharhan <m.taufiq.fharhan@gmail.com>
2025-03-29 06:15:05 +0800

    Variables Introduction

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,46 @@ public class Main {
         System.out.println("I like spaghetti!");
         System.out.println("It's really awesome!");
         System.out.println("Buy me spaghetti!");
+
+        int age = 30;
+
+        System.out.println("Hello my age is " + age);
+
+        double price = 19;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        System.out.println("$" + price);
+
+        char grade = 'A';
+        char symbol = '!';
+
+        System.out.println(symbol);
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        System.out.println(isStudent);
+
+        String name = "Taufiq Fharhan";
+        String food = "spaghetti";
+        String email = "testing123@gmail.com";
+        String car = "Tesla";
+
+        System.out.println("Hello, my name is " + name);
+        System.out.println("Favourite food is " + food);
+        System.out.println("Your email is " + email);
+
+        System.out.println("Hello " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("Your average grade is " + grade);
+
+        if (forSale) {
+            System.out.println("There is a " + car + " for sale");
+        } else {
+            System.out.println("There is a " + car + " not for sale");
+        }
         
     }
 }
